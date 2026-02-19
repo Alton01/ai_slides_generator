424ddee19f123aa30e71c745263efcdacbfa67fa
Alton Obinna <altonobinna11@gmail.com>
2026-02-19 12:23:57 +0100

    index

--- aislides/settings.py
+++ aislides/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'aislides.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', Path(__file__).resolve().parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
